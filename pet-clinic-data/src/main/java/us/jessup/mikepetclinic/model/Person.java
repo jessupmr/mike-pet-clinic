7368233bf56a8d73287c2a3a2f98e3b953cd1712
Mike Jessup <jessupmr@gmail.com>
2020-04-19 15:51:58 -0700

    Implemented Base Entity that has Long id field. Closes #10

--- pet-clinic-data/src/main/java/us/jessup/mikepetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/us/jessup/mikepetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package us.jessup.mikepetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/us/jessup/mikepetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/us/jessup/mikepetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package us.jessup.mikepetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
 
     public PetType getPetType() {
